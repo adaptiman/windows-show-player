a392b22a21ee4dbb5a4db7e9cd3d294e7d53d924
David Sweeney <david@thesweeneys.org>
2021-07-12 08:42:01 -0500

    Initial commit.

---
+++ Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CreateAudioCueToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.flpMain = New System.Windows.Forms.FlowLayoutPanel()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(32, 32)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.ToolsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(2127, 40)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(72, 36)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ToolsToolStripMenuItem
+        '
+        Me.ToolsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CreateAudioCueToolStripMenuItem})
+        Me.ToolsToolStripMenuItem.Name = "ToolsToolStripMenuItem"
+        Me.ToolsToolStripMenuItem.Size = New System.Drawing.Size(90, 36)
+        Me.ToolsToolStripMenuItem.Text = "Tools"
+        '
+        'CreateAudioCueToolStripMenuItem
+        '
+        Me.CreateAudioCueToolStripMenuItem.Name = "CreateAudioCueToolStripMenuItem"
+        Me.CreateAudioCueToolStripMenuItem.Size = New System.Drawing.Size(359, 44)
+        Me.CreateAudioCueToolStripMenuItem.Text = "Create Audio Cue"
+        '
+        'flpMain
+        '
+        Me.flpMain.AutoSize = True
+        Me.flpMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.flpMain.Location = New System.Drawing.Point(0, 40)
+        Me.flpMain.Name = "flpMain"
+        Me.flpMain.Size = New System.Drawing.Size(2127, 1238)
+        Me.flpMain.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(2127, 1278)
+        Me.Controls.Add(Me.flpMain)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CreateAudioCueToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents flpMain As FlowLayoutPanel
+End Class
---
+++ Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    'Indicates current media panel to add controls to
+    Private _CurrentMediaPanelName As String = Nothing
+
+    'Used to give unique control names such as label1, label2, etc.
+    Private _MediaPanelsAddedCount As Integer = 0
+
+    'Add media panel to flow layout panel
+    Public Sub CreateMediaPanel()
+
+        Dim mediaPanel As Panel
+        mediaPanel = New Panel()
+
+        'Set panel properties
+        With mediaPanel
+            .BackColor = Color.White
+            .Size = New Size(256, 144)
+            .Name = "pnlMedia" + (_MediaPanelsAddedCount + 1).ToString
+        End With
+
+        'Add panel to flow layout panel
+        flpMain.Controls.Add(mediaPanel)
+
+        'Update panel variables
+        _CurrentMediaPanelName = mediaPanel.Name
+        _MediaPanelsAddedCount += 1
+
+
+    End Sub
+
+    Private Sub flpMain_Paint(sender As Object, e As PaintEventArgs) Handles flpMain.Paint
+
+    End Sub
+    'Add New media player to media panel
+    Public Sub CreateMediaPlayer(ByVal panelName As String, ByVal mediaPath As String)
+
+        Dim mediaPlayer As AxWMPLib.AxWindowsMediaPlayer
+        mediaPlayer = New AxWMPLib.AxWindowsMediaPlayer
+
+        'Set properties
+        With mediaPlayer
+            .URL = mediaPath
+            .settings.autoStart = False
+            .Name = "mediaPlayer" + (_MediaPanelsAddedCount).ToString
+            '.uiMode = mini
+            '.Size = (130, 40)
+            .Dock = DockStyle.Bottom
+        End With
+
+        'Loop through controls and add new label to passed panel
+        For Each ControlObject As Control In flpMain.Controls
+            If ControlObject.Name = panelName Then
+                ControlObject.Controls.Add(mediaPlayer)
+            End If
+        Next
+    End Sub
+
+
+    'Add new play button to panel
+    Public Sub CreateMediaPlayButton(ByVal panelName As String)
+
+    End Sub
+
+    Private Sub CreateAudioCueToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CreateAudioCueToolStripMenuItem.Click
+
+        'Dim x As Integer = 0
+        'Do
+        '    CreateMediaPanel()
+        '    x += 1
+        'Loop Until x > 50
+
+        CreateMediaPanel()
+
+        CreateMediaPlayer(_CurrentMediaPanelName, "D:\CFP\bumpers\categorical\opening 2010.wav")
+
+
+    End Sub
+End Class
